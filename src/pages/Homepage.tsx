04289e84a379d8b8bb4647d74df51a63de2d4ba6
yasharth-0910 <yasharthsingh0910@gmail.com>
2024-09-10 20:06:54 +0530

    UI fixes

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { RocketIcon, MenuIcon, CpuIcon, LayersIcon, ChevronDownIcon, InstagramIcon, LinkedinIcon, PhoneIcon } from "lucide-react"
 import { useNavigate } from 'react-router-dom';
 
